290c69b50a4b52186a33fb76239040212c5c5f56
rmob <riley.obryan@gmail.com>
2023-03-14 05:43:26 -0700

    addReaction route works

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,12 +5,14 @@ const {
   createThought,
   updateThought,
   deleteThought,
-//   addReaction,
+  addReaction,
 //   removeReaction,
 } = require('../../controllers/thoughtControllers');
 
 router.route('/').get(getThoughts).post(createThought);
 
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction)
 
 module.exports = router;

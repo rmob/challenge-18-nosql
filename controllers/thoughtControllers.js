6bce9407d0cb16b32ef585887bfa1f95bb6781d5
rmob <riley.obryan@gmail.com>
2023-03-14 05:54:32 -0700

    removeReaction route works

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -71,4 +71,21 @@ updateThought(req, res) {
         res.status(500).json(err);
       });
   },
+
+  removeReaction(req, res) {
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } }
+    )
+    .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,13 +6,15 @@ const {
   updateThought,
   deleteThought,
   addReaction,
-//   removeReaction,
+  removeReaction,
 } = require('../../controllers/thoughtControllers');
 
 router.route('/').get(getThoughts).post(createThought);
 
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
-router.route('/:thoughtId/reactions').post(addReaction)
+router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction)
+
+// router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction)
 
 module.exports = router;
